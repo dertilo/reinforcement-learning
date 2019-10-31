2efba4594a1f878d632ab5684f953d9f7bf1235a
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-11-01 00:35:50 +0100

    refactoring

--- minimal_cartpole_example.py
+++ minimal_cartpole_example.py
@@ -10,10 +10,14 @@ import torch.nn.functional as F
 from tqdm import tqdm
 
 
-def epsgreedy_action(num_actions, policy_actions, epsilon):
-    random_actions = torch.randint_like(policy_actions, num_actions)
-    selector = torch.rand_like(random_actions, dtype=torch.float32)
-    return torch.where(selector > epsilon, policy_actions, random_actions)
+def mix_in_some_random_actions(policy_actions, eps, num_actions):
+    if eps > 0.0:
+        random_actions = torch.randint_like(policy_actions, num_actions)
+        selector = torch.rand_like(random_actions, dtype=torch.float32)
+        actions = torch.where(selector > eps, policy_actions, random_actions)
+    else:
+        actions = policy_actions
+    return actions
 
 
 class CartPoleAgent(nn.Module):
@@ -38,10 +42,7 @@ class CartPoleAgent(nn.Module):
     def step_batch(self, obs_batch, eps=0.001):
         q_values = self.calc_q_values(obs_batch)
         policy_actions = q_values.argmax(dim=1)
-        if eps > 0.0:
-            actions = epsgreedy_action(self.num_actions, policy_actions, eps)
-        else:
-            actions = policy_actions
+        actions = mix_in_some_random_actions(policy_actions, eps, self.num_actions)
         return actions
 
     def step_single(self, obs, eps=0.001):
@@ -72,15 +73,12 @@ def experience_generator(agent, env: gym.Env):
         action = agent.step_single(obs)
         for it in range(1000):
             next_obs, _, next_done, info = env.step(action)
-            if next_done:
-                next_reward = -10.0
-            else:
-                next_reward = 1.0
+
             yield {
                 "obs": obs,
                 "next_obs": next_obs,
                 "action": action,
-                "next_reward": next_reward,
+                "next_reward": -10.0 if next_done else 1.0,
                 "next_done": next_done,
             }
 
@@ -92,44 +90,42 @@ def experience_generator(agent, env: gym.Env):
 
 
 def gather_experience(experience_iter: Iterator, batch_size: int = 32):
-    experience = [next(experience_iter) for _ in range(batch_size)]
+    experience_batch = [next(experience_iter) for _ in range(batch_size)]
     exp_arrays = {
-        key: np.array([exp[key] for exp in experience]) for key in experience[0].keys()
+        key: np.array([exp[key] for exp in experience_batch])
+        for key in experience_batch[0].keys()
     }
     return exp_arrays
 
 
-def train_agent(agent: CartPoleAgent, env: gym.Env):
+def train_agent(agent: CartPoleAgent, env: gym.Env, train_steps=3_000):
     optimizer = RMSprop(agent.parameters())
-    train_steps = 3_000
-    discount = 0.99
-
     experience_iterator = iter(experience_generator(agent, env))
-    with tqdm(postfix=[{"avg-reward": 0}]) as pbar:
 
-        for ep in range(train_steps):
-            with torch.no_grad():
-                agent.eval()
-                experience = gather_experience(experience_iterator, batch_size=32)
-                next_q_values = agent.calc_q_values(experience["next_obs"])
-                max_next_value, _ = next_q_values.max(dim=1)
+    for it in tqdm(range(train_steps)):
+        with torch.no_grad():
+            agent.eval()
+            experience = gather_experience(experience_iterator, batch_size=32)
+            estimated_return = calc_estimated_return(agent, experience)
 
-                mask = torch.tensor((1 - experience["next_done"]), dtype=torch.float)
-                next_reward = torch.tensor(experience["next_reward"], dtype=torch.float)
-                estimated_return = next_reward + discount * max_next_value * mask
+        agent.train()
+        q_values = agent.calc_q_values(experience["obs"])
+        actions = torch.tensor(experience["action"]).unsqueeze(1)
+        q_selected = q_values.gather(1, actions).squeeze(1)
 
-            agent.train()
-            q_values = agent.calc_q_values(experience["obs"])
-            actions = torch.tensor(experience["action"]).unsqueeze(1)
-            q_selected = q_values.gather(1, actions).squeeze(1)
+        loss_value = F.mse_loss(q_selected, estimated_return)
+        optimizer.zero_grad()
+        loss_value.backward()
+        optimizer.step()
 
-            loss_value = F.mse_loss(q_selected, estimated_return)
-            optimizer.zero_grad()
-            loss_value.backward()
-            optimizer.step()
 
-            pbar.postfix[0]["avg-reward"] = np.mean(experience["next_reward"])
-            pbar.update()
+def calc_estimated_return(agent, experience, discount=0.99):
+    next_q_values = agent.calc_q_values(experience["next_obs"])
+    max_next_value, _ = next_q_values.max(dim=1)
+    mask = torch.tensor((1 - experience["next_done"]), dtype=torch.float)
+    next_reward = torch.tensor(experience["next_reward"], dtype=torch.float)
+    estimated_return = next_reward + discount * max_next_value * mask
+    return estimated_return
 
 
 if __name__ == "__main__":
