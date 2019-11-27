30be69423ab439e1fce12f82ad7ea62a9df8a945
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-11-27 13:08:38 +0100

    collect_experience

--- reinforce_cartpole_minimal_example.py
+++ reinforce_cartpole_minimal_example.py
@@ -8,16 +8,17 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Categorical
 
-'''
+"""
 based on: https://github.com/pytorch/examples/blob/master/reinforcement_learning/reinforce.py
-'''
+"""
 
 
 class RLparams(NamedTuple):
-    gamma:float=0.99
-    seed:int=543
-    render:bool=False
-    log_interval:int=10
+    gamma: float = 0.99
+    seed: int = 543
+    render: bool = False
+    log_interval: int = 10
+
 
 class Policy(nn.Module):
     def __init__(self):
@@ -34,6 +35,7 @@ class Policy(nn.Module):
         action_scores = self.affine2(x)
         return F.softmax(action_scores, dim=1)
 
+
 def select_action(state):
     state = torch.from_numpy(state).float().unsqueeze(0)
     probs = policy(state)
@@ -43,60 +45,63 @@ def select_action(state):
     return action.item()
 
 
-def finish_episode(args:RLparams):
-    R = 0
-    policy_loss = []
-    returns = []
-    for r in policy.rewards[::-1]:
-        R = r + args.gamma * R
-        returns.insert(0, R)
-    returns = torch.tensor(returns)
-    returns = (returns - returns.mean()) / (returns.std() + eps)
-    for log_prob, R in zip(policy.saved_log_probs, returns):
-        policy_loss.append(-log_prob * R)
-    optimizer.zero_grad()
-    policy_loss = torch.cat(policy_loss).sum()
-    policy_loss.backward()
-    optimizer.step()
-    del policy.rewards[:]
-    del policy.saved_log_probs[:]
+def collect_experience(env: gym.Env):
+    state, ep_reward = env.reset(), 0
+    for t in range(1, 10000):  # Don't infinite loop while learning
+        action = select_action(state)
+        state, reward, done, _ = env.step(action)
+        if args.render:
+            env.render()
+        policy.rewards.append(reward)
+        ep_reward += reward
+        if done:
+            break
+
+    return ep_reward
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = RLparams()
 
     policy = Policy()
     optimizer = optim.Adam(policy.parameters(), lr=1e-2)
     eps = np.finfo(np.float32).eps.item()
 
-    env = gym.make('CartPole-v1')
+    env = gym.make("CartPole-v1")
     env.seed(args.seed)
     torch.manual_seed(args.seed)
 
     running_reward = 0
-    for i_episode in range(600):
-        state, ep_reward = env.reset(), 0
-        for t in range(1, 10000):  # Don't infinite loop while learning
-            action = select_action(state)
-            state, reward, done, _ = env.step(action)
-            if args.render:
-                env.render()
-            policy.rewards.append(reward)
-            ep_reward += reward
-            if done:
-                break
+    episode_counter = 0
+    while running_reward < env.spec.reward_threshold:
+        ep_reward = collect_experience(env)
+
+        R = 0
+        policy_loss = []
+        returns = []
+        for r in policy.rewards[::-1]:
+            R = r + args.gamma * R
+            returns.insert(0, R)
+        returns = torch.tensor(returns)
+        returns = (returns - returns.mean()) / (returns.std() + eps)
+        for log_prob, R in zip(policy.saved_log_probs, returns):
+            policy_loss.append(-log_prob * R)
+        optimizer.zero_grad()
+        policy_loss = torch.cat(policy_loss).sum()
+        policy_loss.backward()
+        optimizer.step()
+        del policy.rewards[:]
+        del policy.saved_log_probs[:]
 
         running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
-        finish_episode(args)
-        if i_episode % args.log_interval == 0:
-            print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
-                  i_episode, ep_reward, running_reward))
-        if running_reward > env.spec.reward_threshold:
-            print("Solved! Running reward is now {} and "
-                  "the last episode runs to {} time steps!".format(running_reward, t))
-            break
 
-
+        episode_counter += 1
+        if episode_counter % args.log_interval == 0:
+            print(
+                "Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}".format(
+                    episode_counter, ep_reward, running_reward
+                )
+            )
 
     while True:
         state, ep_reward = env.reset(), 0
@@ -107,3 +112,7 @@ if __name__ == '__main__':
                 break
             env.render()
 
+
+"""
+Episode 200	Last reward: 500.00	Average reward: 472.84
+"""
