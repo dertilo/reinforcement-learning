34d97a16446556049846b345ed32fe4a5f2f89c0
Tilo Himmelsbach <dertilo@googlemail.com>
2020-06-19 20:00:51 +0200

    use torch not tensorflow

--- kuka_robot.py
+++ kuka_robot.py
@@ -1,8 +1,21 @@
 # based on: examples/pybullet/gym/pybullet_envs/baselines/train_kuka_grasping.py
+from typing import NamedTuple
+
+import gym
+import torch
 from baselines.common.models import mlp
 from pybullet_envs.bullet.kukaGymEnv import KukaGymEnv
 from baselines import deepq
 import pybullet as pb
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+from tqdm import tqdm
+
+eps = np.finfo(np.float32).eps.item()
 
 
 class KukaRobotEnv(KukaGymEnv):
@@ -16,38 +29,106 @@ class KukaRobotEnv(KukaGymEnv):
             self.blockUid, self._kuka.kukaUid, 1000, -1, self._kuka.kukaEndEffectorIndex
         )
         distance = closestPoints[0][8]
-        if distance < self.distance:
-            reward = -1.0
-        else:
-            reward = 1.0
-        self.distance = distance
+        reward = 1.0 / distance
+        # if distance < self.distance:
+        #     reward = -1.0
+        # else:
+        #     reward = 1.0
+        # self.distance = distance
         return reward
 
 
-def callback(lcl, glb):
-    # stop training if reward exceeds 199
-    total = sum(lcl["episode_rewards"][-101:-1]) / 100
-    totalt = lcl["t"]
-    is_solved = totalt > 200 and total >= 100
-    if is_solved:
-        print("is_solved")
-    return is_solved
+class RLparams(NamedTuple):
+    gamma: float = 0.99
+    seed: int = 543
+    render: bool = False
+    log_interval: int = 10
+
+
+class PolicyAgent(nn.Module):
+    def __init__(self, obs_dim, num_actions) -> None:
+        super().__init__()
+        n_hid = 64
+        self.affine1 = nn.Linear(obs_dim, n_hid)
+        self.affine2 = nn.Linear(n_hid, num_actions)
+
+    def forward(self, x):
+        x = self.affine1(x)
+        x = F.relu(x)
+        action_scores = self.affine2(x)
+        return F.softmax(action_scores, dim=1)
+
+    def step(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        probs = self.forward(state)
+        m = Categorical(probs)
+        action = m.sample()
+        return action.item(), m.log_prob(action)
+
+
+def collect_experience(env: gym.Env, agent: PolicyAgent):
+    state, ep_reward = env.reset(), 0
+    exp = []
+    for t in range(1, 10000):  # Don't infinite loop while learning
+        action, log_probs = agent.step(state)
+        state, reward, done, _ = env.step(action)
+        exp.append((action, log_probs, reward))
+        ep_reward += reward
+        if done:
+            break
+
+    return ep_reward, exp
+
+
+def calc_returns(exp, gamma):
+    returns = []
+    R = 0
+    for action, log_prob, r in reversed(exp):
+        R = r + gamma * R
+        returns.insert(0, R)
+    returns = torch.tensor(returns)
+    returns = (returns - returns.mean()) / (returns.std() + eps)
+    return returns
+
+
+def calc_loss(exp, gamma):
+    returns = calc_returns(exp, gamma)
+    policy_loss = [-log_prob * R for (_, log_prob, _), R in zip(exp, returns)]
+    policy_loss = torch.cat(policy_loss).sum()
+    return policy_loss
+
+
+def train(env: KukaRobotEnv, agent: PolicyAgent, args: RLparams):
+
+    num_games = 10
+    for k in tqdm(range(num_games)):
+        ep_reward, exp = collect_experience(env, agent)
+
+        policy_loss = calc_loss(exp, args.gamma)
+
+        optimizer.zero_grad()
+        policy_loss.backward()
+        optimizer.step()
 
 
 if __name__ == "__main__":
 
+    args = RLparams()
     env = KukaRobotEnv(renders=False, maxSteps=1000, isDiscrete=True)
-    model = mlp()
-    act = deepq.learn(
-        env,
-        network=model,
-        lr=1e-3,
-        max_timesteps=10000000,
-        buffer_size=50000,
-        exploration_fraction=0.1,
-        exploration_final_eps=0.02,
-        print_freq=10,
-        callback=callback,
-    )
-    model_file = "/docker-share/data/tmp/kuka_model.pkl"
-    act.save(model_file)
+    agent: PolicyAgent = PolicyAgent(env.observation_space.shape[0], env.action_space.n)
+    optimizer = optim.Adam(agent.parameters(), lr=1e-2)
+    eps = np.finfo(np.float32).eps.item()
+
+    env.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    train(env, agent, args)
+    del env
+    env = KukaRobotEnv(renders=True, maxSteps=1000, isDiscrete=True)
+    while True:
+        state, ep_reward = env.reset(), 0
+        for t in range(1, 10000):  # Don't infinite loop while learning
+            action, _ = agent.step(state)
+            state, reward, done, _ = env.step(action)
+            if done:
+                break
