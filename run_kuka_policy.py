ce890d28e9dbddd013f75fe808a9439347318221
Tilo Himmelsbach <dertilo@googlemail.com>
2020-06-20 11:00:36 +0200

    enjoy kuka

--- run_kuka_policy.py
+++ run_kuka_policy.py
@@ -8,26 +8,27 @@ from baselines.deepq import load_act
 from pybullet_envs.bullet import KukaGymEnv
 from tensorflow.python.saved_model.load import load
 
-from kuka_robot import KukaRobotEnv
+from kuka_robot import KukaRobotEnv, PolicyAgent
 
 
 def enjoy_kuka(model_file):
-  agent = torch.load(model_file)
-  agent.eval()
+    agent:PolicyAgent = torch.load(model_file)
+    agent.eval()
 
-  env = KukaRobotEnv(renders=True, maxSteps=1000, isDiscrete=True)
-  from gym.wrappers import Monitor
-  env = Monitor(env, "./vid",
-                video_callable=lambda episode_id: True, force=True)
-  while True:
-    state, ep_reward = env.reset(), 0
-    for t in range(1, 10000):  # Don't infinite loop while learning
-      action, _ = agent.step(state)
-      state, reward, done, _ = env.step(action)
-      if done:
-        break
+    env = KukaRobotEnv(renders=True, maxSteps=1000, isDiscrete=True)
+    from gym.wrappers import Monitor
+
+    env = Monitor(env, "./vid", video_callable=lambda episode_id: True, force=True)
+    while True:
+        state, ep_reward = env.reset(), 0
+        for t in range(1, 10000):  # Don't infinite loop while learning
+            action, _ = agent.step(state)
+            state, reward, done, _ = env.step(action)
+            if done:
+                break
 
 
-if __name__ == '__main__':
-  model_file = os.environ["HOME"]+"/gunther/data/kuka_models/model.pt"
-  enjoy_kuka(model_file)
+if __name__ == "__main__":
+    model_file = os.environ["HOME"] + "/gunther/data/kuka_models/model.pt"
+    # model_file = os.environ["HOME"] + "/data/kuka_models/model.pt"
+    enjoy_kuka(model_file)
